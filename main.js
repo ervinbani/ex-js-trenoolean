b84b3baac370770b7ab12bc89df7414aa1e7e74a
ervinbani <bani.ervin@gmail.com>
2018-03-26 16:51:41 +0200

    commit

---
+++ main.js
@@ -0,0 +1,56 @@
+/*
+*Trenoolean*
+Un treno è rappresentato dalle seguenti caratteristiche:
+- numero indentifico di 4 cifre orari
+- Stazione di partenza
+- Stazione di arrivo
+- Durata del viaggio in minuti
+- Orario di partenza
+- Numero di posti liberi
+
+A) Generare 10 treni in partenza da Roma e in arrivo a firenze e 10 treni in partenza da milano e in arrivo a Roma
+Non devono essere generati a mano, ma con cicli che creino dei valori plausibili per ogni dato
+
+B) Stampare il numero identificativo e l'orario di partenza di un treno che da Roma e va a Firenze,
+con le seguenti caratteristiche
+ 1) Quello che parte prima
+ 2) Quello che ci mette meno
+ 3) Quello più vuoto
++-
+C) Prenotazione: chiedere al'utente la stazione di partenza, la stazione di arrivo e il modo di ricerca.
+Il modo di ricerca può essere una delle opzioni del punto B.
+Il software deve individuare il treno con le caratteristiche scelte dall'utente che abbiamo posti liberi,
+
+
+
+decrementare il numero di posti liberi e generare un Codice Prenotazione di 6 caratteri alfanumerici casuali.
+*/
+
+//creo un un oggetto treno vuoto
+var treno={
+  "codice_id":" ",
+  "st_partenza":" ",
+  "st_arrivo": " ",
+  "durata":" ",
+  "orario" : " ",
+  "postiLiberi": " "
+}
+
+//creo due array vuoti dove vado a memorizzare i dati dei treni;
+var RomaFirenze=[];
+var MilanoRoma[];
+var i=0;
+do{
+  for(var k=0;k<10;k++ ){
+    
+
+  }
+
+}while(i<10)
+
+
+function generaArray(){
+
+
+
+}
