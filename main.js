1b9e8bdc589a1536a727e28f230658370ec9605d
ervinbani <bani.ervin@gmail.com>
2018-03-27 14:34:34 +0200

    migioramento del codice

--- main.js
+++ main.js
@@ -139,9 +139,6 @@ function alfanumerico(){
   var casualarray=[];
   var array=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P", "Q","R", "S", "T", "U", "V", "W", "X", "Y", "Z",0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  for(var i=0;i<10;i++){
-    array[i]=i;
-  }
   for(var i=0;i<6;i++){
     casualarray.push(array[Math.floor(Math.random()*array.length)+1]);
   }
